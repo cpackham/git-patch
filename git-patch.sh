2ce3c47580edea19a41caa03a6720db3ae4d8ecb
Chris Packham <judge.packham@gmail.com>
2013-06-22 20:35:38 +1200

    Licence GPLv2

--- git-patch.sh
+++ git-patch.sh
@@ -3,6 +3,16 @@
 # Git equivalent of some useful stgit commands
 #
 # Copyright (c) 2013, Chris Packham
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 SUBDIRECTORY_OK="yes"
 OPTIONS_KEEPDASHDASH=
