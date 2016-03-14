88b493afae7ac9a30a7a38ad4329f72cc1f587f8
Chris Packham <judge.packham@gmail.com>
2016-03-14 21:53:18 +1300

    "compile" script
    We were relying on ./configure to generate git-patch from git-patch.sh.
    This worked but it is less than intuitive and meant that changes could
    not be incorporated with "make" after having run ./configure once.
    
    Add rules to generate git-patch using sed.
    
    Signed-off-by: Chris Packham <judge.packham@gmail.com>

--- git-patch.sh
+++ git-patch.sh
@@ -26,7 +26,7 @@ git patch [options] fixup commit-ish [file] [...]
 
 Options:
 --
-version      Print @PACKAGE@ version information
+version      Print @PACKAGE_@ version information
 h,help       Print this help message and exit
 "
 
@@ -37,7 +37,7 @@ while test $# -ne 0
 do
 	case "$1" in
 	--version)
-		say "@PACKAGE@ @VERSION@"
+		say "@PACKAGE_@ @VERSION_@"
 		exit 0
 		;;
 	--)
